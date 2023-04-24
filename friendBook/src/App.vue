d92a06d4f65e75d5e793e1170bdc81d8f0260acd
xlanex6 <alexduval71@gmail.com>
2023-04-24 10:37:02 +0200

    init + name asking

---
+++ friendBook/src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default {
+    data() {
+        return {
+            name: "",
+            showNavBar: false
+        };
+    },
+  components: { Navbar },
+  methods: {
+    async fetchUsers() {
+      const res = await fetch('https://jsonplaceholder.typicode.com/users')
+      this.users = await res.json()
+      debugger
+      }
+    }
+}
+</script>
+
+<template>
+  <Navbar v-if="showNavBar" :name="name"/>
+  
+  <div v-else="!showNavBar" class="grid min-h-screen place-content-center">
+    <input type="text" v-model="name" class="text-center uppercase border rounded-md border-neutral-200" placeholder="name">
+    <button @click="" class="w-full my-3 text-white bg-purple-500 rounded shadow-sm">SAVE</button>
+  </div>
+  
+</template>
+
+<style scoped>
+</style>
---
+++ friendBook/src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+
+  <h1 class="flex justify-end px-4 bg-gray-100">{{ name }}</h1>
+
+</template>
+
+<script>
+export default {
+  props: ['name']
+}
+</script>
