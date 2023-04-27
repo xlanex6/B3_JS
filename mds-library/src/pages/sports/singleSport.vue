fe08a70494894057c8d46cecfc10142ef37f8df9
xlanex6 <alexduval71@gmail.com>
2023-04-27 13:46:29 +0200

    dynamic link

---
+++ mds-library/src/pages/sports/singleSport.vue
@@ -0,0 +1,26 @@
+<script >
+import Header from '../../components/Header.vue';
+
+export default {
+  components: { Header },
+  data() {
+    return {
+      resServer: {
+        image: "https://picsum.photos/1200/300.jpg",
+        baseline: `Venez faire du ${this.$route.params.slug} sur Annecy`,
+        title: "index",
+        link: "http://www.google.com",
+      }
+    }
+  }
+}
+
+</script>
+
+<template>
+  <Header :sportData="resServer">
+  </Header>
+</template>
+
+<style scoped>
+</style>
--- mds-library/src/routes.js
+++ mds-library/src/routes.js
@@ -1,14 +1,18 @@
 
 import Index from './pages/index.vue'
 import Kayak from './pages/kayak.vue'
-import Boxe from './pages/boxe.vue'
+// import Boxe from './pages/boxe.vue'
 import Machin from './pages/machin.vue'
 import Contact from './pages/contact.vue'
+import SportIndex from './pages/sports/index.vue'
+import SingleSport from './pages/sports/singleSport.vue'
 
 const routes = [
   { path: '/', component: Index ,navbarVisible: true , displayName: 'Home'},
-  { path: '/kayak', component: Kayak ,navbarVisible: true , displayName: 'Kayak'},
-  { path: '/boxe', component: Boxe ,navbarVisible: true , displayName: 'Boxe'},
+  { path: '/sports', component: SportIndex ,navbarVisible: true , displayName: 'Sports'},
+  { path: '/sports/:slug', component: SingleSport, navbarVisible: false, displayName: 'SingleSport'},
+  // { path: '/kayak', component: Kayak ,navbarVisible: true , displayName: 'Kayak'},
+  // { path: '/boxe', component: Boxe ,navbarVisible: true , displayName: 'Boxe'},
   { path: '/machin', component: Machin ,navbarVisible: true , displayName: 'Machin'},
   { path: '/contact', component: Contact ,navbarVisible: false , displayName: 'Contact'},
 ]
