2b86b5f7a65937149ce8ec3854fe946d13900d82
xlanex6 <alexduval71@gmail.com>
2023-04-28 09:51:28 +0200

    COMP API

--- nuxt-blog/pages/index.vue
+++ nuxt-blog/pages/index.vue
@@ -19,4 +19,27 @@ useHead({
     },
   ],
 })
+
+// methods 
+function XX() {
+
+}
+
+// computed
+const YY = computed(() => {
+  return  isMajor.value == this.isMajor
+        //   COMP API      options API
+})
+
+// data()
+const newData = reactive({
+  name: 'John',
+  age: 42,
+})
+const isMajor = ref(false)
+const myArray = ref([])
+
+console.log(myArray.value) // []
+console.log(newData.name) // John
+
 </script>
